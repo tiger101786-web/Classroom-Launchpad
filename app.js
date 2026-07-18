77d17f2af2fca5b806c5a246ccc7cd0539da80db
tiger101786-web <tiger101786@gmail.com>
2026-07-17 21:03:21 -0500

    Adjust Colt Run flag base height

--- app.js
+++ app.js
@@ -4639,7 +4639,7 @@ function startColtRunGame() {
       ctx.save();
       ctx.shadowColor = "rgba(255, 82, 21, 0.45)";
       ctx.shadowBlur = 16;
-      ctx.drawImage(transparentFlagFrame, flagX - 31, flag.y - 20, flagDrawW, flagDrawH);
+      ctx.drawImage(transparentFlagFrame, flagX - 31, flag.y - 38, flagDrawW, flagDrawH);
       ctx.restore();
     } else {
       ctx.strokeStyle = "#f4f2f3";
