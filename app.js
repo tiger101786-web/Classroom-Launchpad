06402b505535ad42880220991bcfbd6ca068aa1e
tiger101786-web <tiger101786@gmail.com>
2026-09-11 21:52:05 -0500

    Replace Mrs. Trittel leap animation

--- app.js
+++ app.js
@@ -4333,7 +4333,7 @@ function startColtRunGame() {
   let mrsTrittelIdleIndex = 0;
   const getMrsTrittelIdleVideo = () => mrsTrittelIdleVideos[mrsTrittelIdleIndex];
   const mrsTrittelRunVideo = createDeferredVideo("assets/colt-run-mrs-trittel-run.webm?v=20260910-playable1");
-  const mrsTrittelJumpVideo = createDeferredVideo("assets/colt-run-mrs-trittel-jump.webm?v=20260911-clean-leap1");
+  const mrsTrittelJumpVideo = createDeferredVideo("assets/colt-run-mrs-trittel-jump.webm?v=20260911-best-leap1");
   const mrsTrittelDeathVideo = createDeferredVideo("assets/colt-run-mrs-trittel-death.webm?v=20260911-death1");
   const mrsTrittelCelebrationVideos = [
     createDeferredVideo("assets/colt-run-mrs-trittel-celebration.webm?v=20260911-green-key1"),
